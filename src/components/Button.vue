761c8c4de6467138f670907503d9f4a217fff786
Cyebukayire <peacecyebukayire@gmail.com>
2021-08-26 13:01:29 +0200

    Add header and Button components

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+<button :style="{background: color}" class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String,
+    }
+}
+</script>
+
